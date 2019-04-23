4f02be2f386e8bddb63dd05b68a05c6c46aaf43d
Kevin Kamanda <kevkamanda@gmail.com>
2019-04-23 17:06:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,12 @@
 def my_collect(info)
-if block_given?
+  if block_given?
 collection = []
 i = 0
-while i < collection.length do
-    yield (info)
-  i =+ 1
-  end
-   collection
+    while i < collection.length do
+      yield (info)
+      i =+ 1
+    end
+ collection
 end
 
 
